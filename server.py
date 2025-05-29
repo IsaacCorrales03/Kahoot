75c1cbd7b3b0c38df9ce71b59fbd4ee894a75b63
Isaac <isaac.corrales0304@gmail.com>
2025-05-29 09:29:07 -0600

    Todo está en inglés

--- server.py
+++ server.py
@@ -306,4 +306,4 @@ def handle_disconnect():
             del rooms[room_code]
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
